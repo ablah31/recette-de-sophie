ddf42a95adf73949b8599cc8aca4539950fac5d2
ablah31 <114444298+ablah31@users.noreply.github.com>
2024-01-29 14:51:52 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,49 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Charger le fichier JSON
+    fetch('recette.json') // Modifiez le chemin en conséquence si nécessaire
+      .then(response => response.json())
+      .then(data => {
+        const recettesContainer = document.getElementById('recettes-container');
+        const searchInput = document.getElementById('search');
+  
+        // Afficher toutes les recettes au chargement initial
+        afficherRecettes(data);
+  
+        // Ajouter un écouteur d'événements pour la recherche
+        searchInput.addEventListener('input', function() {
+          const searchTerm = searchInput.value.toLowerCase();
+          const recettesFiltrees = data.filter(recette =>
+            recette.Recette.toLowerCase().includes(searchTerm) ||
+            recette.Catégorie.toLowerCase().includes(searchTerm) ||
+            recette.Source.toLowerCase().includes(searchTerm)
+          );
+  
+          // Afficher les recettes filtrées
+          afficherRecettes(recettesFiltrees);
+        });
+      })
+      .catch(error => console.error('Erreur de chargement du fichier JSON:', error));
+  
+    // Fonction pour afficher les recettes
+    function afficherRecettes(recettes) {
+      const recettesContainer = document.getElementById('recettes-container');
+      recettesContainer.innerHTML = '';
+  
+      recettes.forEach(recette => {
+        const recetteDiv = document.createElement('div');
+        recetteDiv.classList.add('recette');
+  
+        recetteDiv.innerHTML = `
+          <h2>${recette.Recette}</h2>
+          <p class="categorie">Catégorie: ${recette.Catégorie}</p>
+          <p>Source: ${recette.Source}</p>
+          <p class="etoiles">Nombre d'étoiles: ${recette["Nombre d'étoiles"]}</p>
+          <p class="commentaires">Commentaires: ${recette.Commentaires}</p>
+          <p class="ingredients">Ingrédients: ${recette.Ingrédients}</p>
+        `;
+  
+        recettesContainer.appendChild(recetteDiv);
+      });
+    }
+  });
+  
